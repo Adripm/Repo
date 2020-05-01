9d76cc5908e5e98f6db4243fae78e70fecd24140
Adrián Palmero <adripalmeromartinez@gmail.com>
2020-05-01 04:44:29 +0200

    new commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from testing import *
+
+test = Test()
+test.shout()
+
+print("this is master")
+print("more shit")
+
+print("some edits to branch")
+
+print("some new shit")
+
+print("editing")
+print("yes")
---
+++ src/testing.py
@@ -0,0 +1,10 @@
+class Test:
+    def __init__(self):
+        pass
+
+    def shout(self):
+        print("FUCK OFF")
+        print("really really into shouting so loud")
+
+    def things(self):
+        pass
